30c1f723514ad855925d77a100cf6b6726e7e655
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-03-13 00:05:22 +0600

    final commit

--- ListTest.java
+++ ListTest.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class ListTest {
     private static final String[] colors1={
@@ -25,7 +26,57 @@ public class ListTest {
         {
             list2.add(color);
         }
+
+        list1.addAll(list2);
+        list2=null;  // no need of list2
+        printList(list1);
+
+        convertToUppercaseStrings(list1);
+        printList(list1);
+
+        System.out.println("deleting elements from 4 to 6");
+        removeItems(list1,4,7);  // 4 inclusive , 7 exclusive
+        printList(list1);
+        printReversedList(list1);
+    }
+
+    // implementation of printList function
+    public void printList(List<String>list)
+    {
+        System.out.println("list: ");
+        for(String color:list)
+            System.out.print(color+" ");
+        System.out.println("");
+    }
+
+    private void convertToUppercaseStrings(List<String>list)
+    {
+        ListIterator<String>iterator= list.listIterator();
+
+        while (iterator.hasNext())
+        {
+            String color= iterator.next();
+            iterator.set(color.toUpperCase());
+        }
+    }
+
+    private void removeItems(List<String>list,int start,int end)
+    {
+        list.subList(start,end).clear();
+    }
+
+    public void printReversedList(List<String>list)
+    {
+        ListIterator<String>iterator= list.listIterator(list.size());
+
+        while (iterator.hasPrevious())
+        {
+            System.out.print(iterator.previous()+" ");
+        }
     }
 
 
+    public static void main(String[] args) {
+        new ListTest();
+    }
 }
